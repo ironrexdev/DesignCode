b4d03c23af73e6c28e1d5ad089c5f2017c160711
AndreyZheleznyak <zheleznyak.dev@gmail.com>
2023-04-17 09:36:13 +0300

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by admin on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
