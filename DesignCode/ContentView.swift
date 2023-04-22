bd34aceaee7a38cb3316d8df39b087d25cf67a23
Andrey Zheleznyak <zheleznyak.dev@gmail.com>
2023-04-22 21:07:49 +0300

    feat: add new gesture and logic for BottomCardView(), add Drag Anchors

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero
     @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
     
     var body: some View {
         ZStack {
@@ -22,8 +24,8 @@ struct ContentView: View {
                     Animation
                         .default
                         .delay(0.1),
-   ///                     .speed(2)
-   ///                     .repeatForever(autoreverses: true),
+                    ///                     .speed(2)
+                    ///                     .repeatForever(autoreverses: true),
                     value: showCard
                 )
             
@@ -60,7 +62,7 @@ struct ContentView: View {
             CardView()
                 .frame(width: showCard ? 375 : 340.0, height: 220.0)
                 .background(Color.black)
-///                .cornerRadius(20)
+            ///                .cornerRadius(20)
                 .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
                 .shadow(radius: 20)
                 .offset(x: viewState.width, y: viewState.height)
@@ -81,10 +83,38 @@ struct ContentView: View {
                         }
                 )
             
+      ///      Text("\(bottomState.height)").offset(y: -300)
+            
             BottomCardView()
                 .offset(x: 0, y: showCard ? 360 : 1000)
+                .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
                 .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8), value: showCard)
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.bottomState = value.translation
+                        if self.showFull {
+                            self.bottomState.height += -300
+                        }
+                        if self.bottomState.height < -300 {
+                            self.bottomState.height = -300
+                        }
+                    }
+                        .onEnded { value in
+                            if self.bottomState.height > 50 {
+                                self.showCard = false
+                            }
+                            if (self.bottomState.height < -100 &&
+                                !self.showFull) || (self.bottomState.height <
+                                -250 && self.showFull) {
+                                self.bottomState.height = -300
+                                self.showFull = true
+                            } else {
+                                self.bottomState = .zero
+                                self.showFull = false
+                            }
+                        }
+                )
         }
     }
 }
