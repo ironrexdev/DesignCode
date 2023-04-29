44856fd16ff225e972c688ca88b7be4931003c20
Andrey Zheleznyak <zheleznyak.dev@gmail.com>
2023-04-29 11:00:03 +0300

    feat: add animation for moving the menu and making it bounce back to the initial position on drag release

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct Home: View {
     
     @State var showProfile = false
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -41,18 +42,30 @@ struct Home: View {
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
+            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
             .scaleEffect(showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: showProfile)
             .edgesIgnoringSafeArea(.all)
             
             MenuView()
                 .background(Color.black.opacity(0.001))
-                .offset( y: showProfile ? 0 : 1000)
+                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: showProfile)
                 .onTapGesture {
                     self.showProfile.toggle()
                 }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                    }
+                        .onEnded { value in
+                           if self.viewState.height > 50 {
+                                self.showProfile = false
+                            }
+                            self.viewState = .zero
+                        }
+                )
         }
     }
 }
